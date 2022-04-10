7a3a28ffb507adf4ebdb8ad1eb474bf1a5c9cc46
narlamanikanta <88223271+narlamanikanta@users.noreply.github.com>
2022-04-10 18:27:56 +0530

    Update footer.php

--- proj/includes/footer.php
+++ proj/includes/footer.php
@@ -2,7 +2,7 @@
   
   <footer class="py-5 bg-inverse">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; BloodBank & Donor Management System 2017</p>
+            <p class="m-0 text-center text-white">Copyright &copy; BloodBank & Donor Management System 2022</p>
         </div>
         <!-- /.container -->
     </footer>
